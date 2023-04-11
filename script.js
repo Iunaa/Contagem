53201e05d1e3c0cd600db40c2759fba6732c2e1b
iuna Leon <yuna.ads@hotmail.com>
2023-04-11 09:26:30 -0300

    assembling js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function carregar(){
-  
+   let start = document.getElementById('start')
+   let finish = document.getElementById('finish')
+   let step = document.getElementById('step')
+   let count = ''
 
 }
 
