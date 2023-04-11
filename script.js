307238b6e7a0c8f9b8a245316ea635b8ef472923
iuna Leon <yuna.ads@hotmail.com>
2023-04-11 11:36:30 -0300

    simple js

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-function carregar(){
+function contar(){
    let start = document.getElementById('start')
    let finish = document.getElementById('finish')
    let step = document.getElementById('step')
-   let count = ''
+   let res = document.getElementById('res')
+
+   if(start.value.length == 0 || step.value.length == 0){
+    window.alert('[ERRO] Faltam dados!')
+   }else{
+    res.innerHTML = 'Contando: '
+    let i = Number(start.value)
+    let f = Number(finish.value)
+    let s = Number(step.value)
+
+    for(let c = i; c <= f; c = c + s){
+        res.innerHTML += `${c}`
+    }
+   }
+  
+
+   
 
 }
 
